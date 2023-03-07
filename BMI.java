a8f69b950b82d363adaccae4c4bddcfc30436f5c
usmanahmed24 <123124209+usmanahmed24@users.noreply.github.com>
2023-03-07 14:28:43 -0500

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class BMI {
+    public static boolean isNumber(String z) {
+        for (int i = 0; i < z.length(); i++) {
+            if (!Character.isDigit(z.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static double BMI(double height, double weight) {
+        return weight / (height * height) * 703;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Name: Usman Ahmed");
+        System.out.println("PID#: 002-48-7071");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = in.next();
+        System.out.println("Please enter you PID#: ");
+        String PID = in.next();
+        System.out.println("Please enter your height");
+        String height = in.nextLine();
+        while (true) {
+            if (isNumber(height)) {
+                System.out.println("The height is:" + height);
+                break;
+            } else {
+                System.out.println("Enter again");
+                height = in.nextLine();
+            }
+        }
+            System.out.println("Please enter your weight");
+        String weight = in.nextLine();
+        while (true) {
+            if (isNumber(weight)) {
+                System.out.println("The weight is:" + weight);
+                break;
+                } else {
+                System.out.println("Enter again");
+                weight = in.nextLine();
+                }
+            }
+        double h = Double.parseDouble(height);
+        double w = Double.parseDouble(weight);
+        double bmi = BMI(h, w);
+        if (bmi < 18.5) {
+            System.out.println("Underweight");
+        }
+        if (bmi >= 18.5 && bmi <=24.9){
+            System.out.println("normal");
+        }
+        if (bmi >= 25.0 && bmi <= 29.9){
+            System.out.println("overweight");
+        }
+        else {
+            System.out.println("Obese");
+        }
+    }
+}
