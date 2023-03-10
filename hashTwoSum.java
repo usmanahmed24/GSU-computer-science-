14792b91c6db3a6362f618718f504aa4de259a80
usmanahmed24 <123124209+usmanahmed24@users.noreply.github.com>
2023-03-10 17:58:43 -0500

    Add files via upload

---
+++ hashTwoSum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class hashTwoSum {
+    public static void main(String[] args) {
+        twoSum(new int[]{2,6,7,8}, 10);
+    }
+
+    public static void twoSum(int[] numbers, int targetSum) {
+        int[] result = null;
+        Set<Integer> map = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int diff = targetSum - numbers[i];
+            if (map.contains(diff)) {
+                result = new int[2];
+                result[0] = numbers[i];
+                result[1] = diff;
+                break;
+            }
+            map.add(numbers[i]);
+        }
+        System.out.println("(" + result[0] + "," + result[1] + ")");
+
+    }
+}
